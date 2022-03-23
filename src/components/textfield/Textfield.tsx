f25e06bc9ed122420911d3e606331517a9da6991
jmrrgncpz <paz.jesr@gmail.com>
2022-03-23 18:47:27 +0800

    implement text fields

--- src/components/textfield/Textfield.tsx
+++ src/components/textfield/Textfield.tsx
@@ -1,9 +1,17 @@
-import { ForwardedRef, forwardRef, HTMLAttributes } from "react";
+import "./Textfield.sass";
+import { ForwardedRef, forwardRef, HTMLAttributes, ReactNode } from "react";
 
-type TextfieldProps = HTMLAttributes<HTMLInputElement>;
+type TextfieldProps = HTMLAttributes<HTMLInputElement> & {
+	inputAdornmentName: string;
+};
 
-const Textfield = forwardRef((props: TextfieldProps, ref: ForwardedRef<HTMLInputElement>) => {
-	return <input {...props} ref={ref} />;
+const Textfield = forwardRef(({ inputAdornmentName, ...rest}: TextfieldProps, ref: ForwardedRef<HTMLInputElement>) => {
+	return (
+		<div className="textfield-root">
+			<span className="material-icons textfield-input-adornment">{inputAdornmentName}</span>
+			<input className="textfield-input" {...rest} ref={ref} />
+		</div>
+	);
 });
 
 export default Textfield;
--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,10 +1,13 @@
 import Textfield from "../components/textfield/Textfield";
 
 const LoginScreen = () => {
-	return <div>
-    <h1>Login</h1>
-    <Textfield />
-  </div>;
+	return (
+		<div>
+			<h1>Login</h1>
+			<Textfield inputAdornmentName="email" placeholder="Email Address" />
+			<Textfield inputAdornmentName="new_releases" placeholder="Password" />
+		</div>
+	);
 };
 
 export default LoginScreen;
